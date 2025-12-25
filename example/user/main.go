37aaf0756237ce8bbe4195a95e0eff75aa5e6f9b
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-12-24 20:47:04 -0500

    Implement user transport and error handling in the SDK

---
+++ example/user/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	userSdk "github.com/JuD4Mo/go_api_web_sdk/user"
+)
+
+func main() {
+	userTransport := userSdk.NewHttpClient("http://localhost:8081", "")
+
+	user, err := userTransport.Get("fa81b5ab-de0b-4d9e-aabc-cb4fe0b58597")
+	if err != nil {
+		if errors.As(err, &userSdk.ErrNotFound{}) {
+			fmt.Println("Not found:", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Internal Server Error:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(user)
+}
---
+++ user/transport.go
@@ -0,0 +1,70 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/JuD4Mo/go_api_web_domain/domain"
+	"github.com/ncostamagna/go_http_client/client"
+)
+
+type (
+	DataResponse struct {
+		Message string      `json:"message"`
+		Code    int         `json:"code"`
+		Data    interface{} `json:"data"`
+		Meta    interface{} `json:"meta"`
+	}
+
+	Transport interface {
+		Get(id string) (*domain.User, error)
+	}
+
+	clientHttp struct {
+		client client.Transport
+	}
+)
+
+func NewHttpClient(baseURL, token string) Transport {
+	header := http.Header{}
+
+	if token != "" {
+		header.Set("Authorization", token)
+	}
+
+	return &clientHttp{
+		client: client.New(header, baseURL, 5000*time.Millisecond, true),
+	}
+}
+
+func (c clientHttp) Get(id string) (*domain.User, error) {
+	dataResponse := DataResponse{Data: &domain.User{}}
+
+	u := url.URL{}
+
+	u.Path += fmt.Sprintf("/users/%s", id)
+
+	reps := c.client.Get(u.String())
+
+	if reps.Err != nil {
+		return nil, reps.Err
+	}
+
+	if reps.StatusCode > 404 {
+		return nil, ErrNotFound{fmt.Sprintf("%s", reps)}
+	}
+
+	if reps.StatusCode > 299 {
+		return nil, fmt.Errorf("%s", reps)
+	}
+
+	err := reps.FillUp(&dataResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return dataResponse.Data.(*domain.User), nil
+
+}
